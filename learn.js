4913169e0f0dac48eb6ff946e04bf9fcd3aa8603
Aviral Srivastava <72144346+godofgeeks23@users.noreply.github.com>
2024-05-21 23:04:52 +0530

    Update learn.js

--- learn.js
+++ learn.js
@@ -1,8 +1,4 @@
-let video;
-let poseNet;
-let pose;
-let skeleton;
-let thirtysecs;
+let video, poseNet, pose, skeleton ,thirtysecs;
 let posesArray = ['Mountain', 'Tree', 'Downward Dog', 'Warrior I', 'Warrior II', 'Chair'];
 var imgArray = new Array();
 
